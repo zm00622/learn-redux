97151eb8f26e08e03138ebc421bf0bd4e86a9d21
zm00622 <zarymanning@gmail.com>
2021-05-07 12:31:06 -0400

    This app creates a basic redux store.

--- src/index.js
+++ src/index.js
@@ -3,15 +3,33 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// import * as serviceWorker from './serviceWorker';
+import {createStore} from 'redux';
+import allReducers from './reducers';
+import { Provider } from 'react-redux';
+
+const store = createStore(
+  allReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
-  <React.StrictMode>
+  // <React.StrictMode> 
+  // Provider (imported above) allows you to access the store inside the app.
+  <Provider store={store}> 
     <App />
-  </React.StrictMode>,
+  </Provider>
+  // </React.StrictMode>,
+  ,
   document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
+// serviceWorker.unregister();
+
+
+
